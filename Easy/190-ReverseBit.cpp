2ba4c0e156107286fcc343250c8b7035365b3cbd
Shazal Inaam <engineershazal55@gmail.com>
2026-06-22 20:12:40 +0500

    190. Reverse Bits Solved

---
+++ Easy/190-ReverseBit.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int reverseBits(int n) {
+    int result = 0;
+    for(int i = 0; i < 32; i++){
+        result <<=1;
+        result = result | (n&1);
+        n >>=1;
+    }
+    return result;
+}
+int main(){
+    int n = 43261596;
+    cout<<reverseBits(n)<<endl;
+    return 0;
+}
