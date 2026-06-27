35197a00a7ce866e313bc102e62020b827c1ca08
Shazal Inaam <engineershazal55@gmail.com>
2026-06-27 15:15:08 +0500

    Problem 852: Peak Index in Mountain Array Solved

---
+++ Medium/852-Peak_Index_in_Mountain_Array.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int peakIndexInMountainArray(vector<int>& arr) {
+    int left = 1;
+    int right = arr.size()-1;
+    while(left <= right){
+        int mid = left + (right - left)/2;
+        if(arr[mid]>arr[mid - 1] && arr[mid]>arr[mid+1]){
+            return mid;
+        }
+        else if(arr[mid] > arr[mid - 1] ){
+            left = mid+1;
+        }
+        else{
+            right = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    vector<int> vec = {3,5,3,2,0};
+    cout<<peakIndexInMountainArray(vec)<<endl;
+}
