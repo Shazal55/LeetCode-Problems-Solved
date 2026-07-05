b436b91a1443436a2cc18dd859c2fef0c82c9b5f
Shazal Inaam <engineershazal55@gmail.com>
2026-07-05 20:40:09 +0500

    More Algorithm added

---
+++ Algorithms/Counting_Sort.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int findKthLargest(vector<int>& nums, int k) {
+        vector<int> count(20001,0);
+        for(int num : nums){
+            count[num + 10000]++;
+        }
+        for(int i = 20000; i>=0; i--){
+            k -= count[i];
+            if(k <= 0){
+                return i - 10000;
+            }
+        }
+        return -1;
+    }
+int main(){
+    vector<int> vec = {3,2,1,5,6,4};
+    int k = 2;
+    cout<<findKthLargest(vec,k)<<endl;
+    return 0;
+}
