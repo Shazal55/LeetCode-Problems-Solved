a8894e4b0f5541453a22d3456973624b5a7b2172
Shazal Inaam <engineershazal55@gmail.com>
2026-08-17 16:22:48 +0500

    Problem 146,556 solved

---
+++ Medium/556-Next_Greater_Element_III.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int nextGreaterElement(int n) {
+    string s = to_string(n);
+    int i = s.size()-2;
+    while(i>= 0 && s[i]>=s[i+1]){
+        i--;
+    }
+    if(i<0){
+        return -1;
+    }
+    int j = s.size()-1;
+    while(s[j]<= s[i]){
+        j--;
+    }
+    swap(s[i],s[j]);
+    reverse(s.begin()+i+1,s.end());
+    long long ans = stoll(s);
+    if(ans > INT32_MAX){
+        return -1;
+    }
+    return int(ans);
+}
+int main(){
+    int n = 12354;
+    cout<<nextGreaterElement(n)<<endl;
+    return 0;
+}
