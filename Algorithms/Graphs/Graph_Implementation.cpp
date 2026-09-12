8b5774ca067908cbd5086248d1968a04c57b56ab
Shazal Inaam <engineershazal55@gmail.com>
2026-09-12 18:25:35 +0500

    Cycle Detection added

--- Algorithms/Graphs/Graph_Implementation.cpp
+++ Algorithms/Graphs/Graph_Implementation.cpp
@@ -67,20 +67,49 @@ public:
         
         cout<<endl;
     }
+    bool isCycle(int src, int parent, vector<bool> &vis){
+        vis[src] = true;
+        list<int> neighbours = arr[src];
+        for(int v : neighbours){
+            if(!vis[v]){
+                if(isCycle(v,src,vis)){
+                    return true;
+                }
+            }
+            else if(v != parent){
+                return true;
+            }
+        }
+        return false;
+    }
+    bool CycleDetection_DFS(){ //O(V+E)
+        vector<bool> vis(V,false);
+        for(int i = 0; i<V; i++){
+            if(!vis[i]){
+                if(isCycle(i,-1,vis)){
+                    return true;
+                }
+            }
+        } 
+        return false;
+    }
 };
 int main(){
     vector<bool> vis();
     Graph g(5);
     g.addEdge(0,1);
+    g.addEdge(0,2);
+    g.addEdge(0,3);
     g.addEdge(1,2);
-    g.addEdge(1,3);
-    g.addEdge(2,4);
+    g.addEdge(3,4);
 
-    g.printAdjacencyList();
-    cout<<"Breadth First Search Traversal :- ";
-    g.BFSTraversal();
-    cout<<"Depth First Search Traversal :- ";
-    g.DFSTraversal();
+    // g.printAdjacencyList();
+    // cout<<"Breadth First Search Traversal :- ";
+    // g.BFSTraversal();
+    // cout<<"Depth First Search Traversal :- ";
+    // g.DFSTraversal();
+
+    cout << g.CycleDetection_DFS()<<endl;
     return 0;
 
 }
@@ -93,4 +122,5 @@ Output:-
 4 : 2
 Breadth First Search Traversal :- 0 1 2 3 4 
 Depth First Search Traversal :- 0 1 2 4 3 
+1
 */
