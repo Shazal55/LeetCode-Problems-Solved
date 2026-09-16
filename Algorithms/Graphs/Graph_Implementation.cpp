3261fd67e7b7fd2c35cf150c373a452308b14816
Shazal Inaam <engineershazal55@gmail.com>
2026-09-16 16:45:00 +0500

    Problem 994 added

--- Algorithms/Graphs/Graph_Implementation.cpp
+++ Algorithms/Graphs/Graph_Implementation.cpp
@@ -18,6 +18,9 @@ public:
         arr[v].push_back(u);
 
     }
+    void addEdge_directed(int u , int v){
+        arr[u].push_back(v);
+    }
     void printAdjacencyList(){
         for(int i = 0; i<V; i++){
             cout<<i <<" : ";
@@ -65,7 +68,7 @@ public:
         }
         cout<<endl;
     }
-    // Cyclle detection using DFS
+    // Cyclle detection for undirected graph using DFS
     bool isCycle(int src, int parent, vector<bool> &vis){
         vis[src] = true;
         list<int> neighbours = arr[src];
@@ -92,7 +95,7 @@ public:
         } 
         return false;
     }
-    //Cycle Detection using BFS
+    //Cycle Detection for undirected graph using BFS
     bool isCycle_BFS_helper(int src, vector<bool> &vis){
         queue<pair<int,int>> q;
         q.push({src,-1});
@@ -125,15 +128,47 @@ public:
         }
         return false;
     }
+    //Cycle detection for directed graph using dfs
+    bool isCycle_Directed_DFS_helper(int curr, vector<bool>&vis, vector<bool> &recPath){
+        vis[curr] = true;
+        recPath[curr] = true;
+
+        for(int v : arr[curr]){
+            if(!vis[v]){
+                if(isCycle_Directed_DFS_helper(v,vis,recPath)){
+                    return true;
+                }
+            }
+            else if( recPath[v]){
+                return true;
+            }
+            
+        }
+        recPath[curr] = false;
+        return false;
+    }
+    bool isCycle_Directed_DFS(){
+        vector<bool> vis(V,false);
+        vector<bool> recPath(V,false);
+
+        for(int i=0; i<V; i++){
+            if(!vis[i]){
+                if(isCycle_Directed_DFS_helper(i,vis,recPath)){
+                    return true;
+                }
+            }
+            
+        }
+        return false;
+    }
 };
 int main(){
-    vector<bool> vis(5,false);
+
     Graph g(5);
-    g.addEdge(0,1);
-    g.addEdge(0,2);
-    g.addEdge(0,3);
-    g.addEdge(1,2);
-    g.addEdge(3,4);
+    g.addEdge_directed(1,0);
+    g.addEdge_directed(0,2);
+    g.addEdge_directed(2,3);
+    g.addEdge_directed(3,0);
 
     // g.printAdjacencyList();
     // cout<<"Breadth First Search Traversal :- ";
@@ -141,18 +176,7 @@ int main(){
     // cout<<"Depth First Search Traversal :- ";
     // g.DFSTraversal();
 
-    cout << g.isCycle_BFS()<<endl;
+    cout << g.isCycle_Directed_DFS()<<endl;
     return 0;
 
 }
-/*
-Output:- 
-0 : 1 
-1 : 0 2 3 
-2 : 1 3 4 
-3 : 1 2 
-4 : 2
-Breadth First Search Traversal :- 0 1 2 3 4 
-Depth First Search Traversal :- 0 1 2 4 3 
-1
-*/
